8b74cba2a3a3e8efe96a580e74b724343144804f
Luy  Studios <luy8462@gmail.com>
2024-12-23 19:05:23 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+const responsive = document.querySelector(".responsive");
+const nav = document.querySelector(".nav");
+
+responsive.addEventListener("click", () =>
+nav.classList.toggle("active"));
